aece3f51dbdbf25058d66ed87a070f6079dcc518
Jørgen Kvalsvik <jokva@statoil.com>
2019-01-18 14:47:57 +0100

    Add list support

--- add.py
+++ add.py
@@ -12,11 +12,19 @@ def addnum(lhs, rhs):
         raise TypeError('right-hand-side must be int')
     return lhs + rhs
 
+def addlist(x, y):
+    return [x] + y
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'Add things')
     parser.add_argument('lhs', help = 'Left-hand side')
     parser.add_argument('rhs', help = 'Right-hand-side')
+    parser.add_argument('tail', type = int, nargs = '*', help = 'More')
 
     args = parser.parse_args()
 
-    print(addnum(args.lhs, args.rhs))
+    out = addnum(args.lhs, args.rhs)
+    if args.tail:
+        out = addlist(out, args.tail)
+
+    print(out)
