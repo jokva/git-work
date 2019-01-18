c7357304316d102afbbd47f7cf6ab20b22abc5ef
Jørgen Kvalsvik <jokva@statoil.com>
2019-01-18 14:44:36 +0100

    Add with --patch, and something to branch from

--- add.py
+++ add.py
@@ -2,5 +2,16 @@
 Add things
 """
 
+import argparse
+
 def addnum(x, y):
     return x + y
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description = 'Add things')
+    parser.add_argument('lhs', help = 'Left-hand side')
+    parser.add_argument('rhs', help = 'Right-hand-side')
+
+    args = parser.parse_args()
+
+    print(addnum(args.lhs, args.rhs))
