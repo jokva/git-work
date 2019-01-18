b0dec88e024528c9c815726aa1b011024376e272
Jørgen Kvalsvik <jokva@statoil.com>
2019-01-18 14:47:57 +0100

    Add list support

--- add.py
+++ add.py
@@ -7,11 +7,19 @@ import argparse
 def addnum(x, y):
     return x + y
 
+def addlist(x, y):
+    return [x] + y
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'Add things')
     parser.add_argument('lhs', help = 'Left-hand side')
     parser.add_argument('rhs', help = 'Right-hand-side')
+    parser.add_argument('tail', type = int, nargs = '*', help = 'More')
 
     args = parser.parse_args()
 
-    print(addnum(args.lhs, args.rhs))
+    out = addnum(args.lhs, args.rhs)
+    if args.tail:
+        out = addlist(out, args.tail)
+
+    print(out)
